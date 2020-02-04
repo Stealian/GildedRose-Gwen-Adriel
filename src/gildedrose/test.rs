521b6f048d9073cc310985fcd7077d356ded6a95
Leirda01 <adrieldj@orange.fr>
2020-02-04 10:13:30 +0100

    Écris les premiers tests
    Il faudrait qu’on arrête de « push » sur `master`…

--- src/gildedrose/test.rs
+++ src/gildedrose/test.rs
@@ -1,10 +1,67 @@
 use super::{Item, GildedRose};
 
+// +5 Dexterity Vest
+// Aged Brie
+// Elixir of the Mongoose
+// Sulfuras, Hand of Ragnaros
+// Backstage passes to a TAFKAL80ETC concert
+
 #[test]
-pub fn foo() {
-    let items = vec![Item::new(String::from("foo"), 0, 0)];
+pub fn sellin_always_decrease_each_day() {
+    let items = vec![Item::new(String::from("+5 Dexterity Vest"), 4, 0),
+                     Item::new(String::from("+5 Dexterity Vest"), -4, 0)];
     let mut rose = GildedRose::new(items);
     rose.update_quality();
 
-    assert_eq!("fixme", rose.items[0].name);
+   assert_eq!(3, rose.items[0].sell_in); 
+   assert_eq!(-5, rose.items[1].sell_in); 
+}
+
+#[test]
+pub fn quality_decrease_by_1_each_day_before_peremption() {
+    let items = vec![Item::new(String::from("+5 Dexterity Vest"), 4, 1)];
+    let mut rose = GildedRose::new(items);
+    rose.update_quality();
+
+    assert_eq!(0, rose.items[0].quality);
+}
+
+#[test]
+pub fn quality_decrease_by_2_each_day_after_peremption() {
+    let items = vec![Item::new(String::from("+5 Dexterity Vest"), -4, 1)];
+    let mut rose = GildedRose::new(items);
+    rose.update_quality();
+
+    assert_eq!(0, rose.items[0].quality);
+}
+
+#[test]
+pub fn quality_cannot_be_negative() {
+    let items = vec![Item::new(String::from("+5 Dexterity Vest"), 4, 0),
+                     Item::new(String::from("+5 Dexterity Vest"), -4, 1)];
+    let mut rose = GildedRose::new(items);
+    rose.update_quality();
+
+    assert_eq!(0, rose.items[0].quality);
+    assert_eq!(0, rose.items[1].quality);
+}
+
+#[test]
+pub fn aged_brie_quality_increase_by_time() {
+    let items = vec![Item::new(String::from("Aged Brie"), 4, 0)];
+    let mut rose = GildedRose::new(items);
+    rose.update_quality();
+
+    assert_eq!(1, rose.items[0].quality);
+}
+
+#[test]
+pub fn sulfuras_quality_never_decrease() {
+    let items = vec![Item::new(String::from("Sulfuras, Hand of Ragnaros"), 0, 80)];
+    let mut rose = GildedRose::new(items);
+    for _i in 0..130 {
+        rose.update_quality();
+    }
+
+    assert_eq!(80, rose.items[0].quality);
 }
