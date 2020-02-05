146e06d10b56fcc900ac8544f53022531a738df4
Stealian <gwenaelle.duchemin.pat@gmail.com>
2020-02-05 14:59:09 +0100

    remplace a = a-1 par a -=1 dans tout le fichier

--- src/gildedrose/mod.rs
+++ src/gildedrose/mod.rs
@@ -27,24 +27,23 @@ impl GildedRose {
             if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
                 if item.quality > 0 {
                     if item.name != "Sulfuras, Hand of Ragnaros" {
-                        item.quality = item.quality - 1;
+                        item.quality -= 1;
                     }
                 }
             } else {
-                if item.quality < 50
-                {
-                    item.quality = item.quality + 1;
+                if item.quality < 50 {
+                    item.quality += 1;
 
                     if item.name == "Backstage passes to a TAFKAL80ETC concert" {
                         if item.sell_in < 11 {
                             if item.quality < 50 {
-                                item.quality = item.quality + 1;
+                                item.quality += 1;
                             }
                         }
 
                         if item.sell_in < 6 {
                             if item.quality < 50 {
-                                item.quality = item.quality + 1;
+                                item.quality += 1;
                             }
                         }
                     }
@@ -52,7 +51,7 @@ impl GildedRose {
             }
 
             if item.name != "Sulfuras, Hand of Ragnaros" {
-                item.sell_in = item.sell_in - 1;
+                item.sell_in -= 1;
             }
 
             if item.sell_in < 0 {
@@ -60,15 +59,15 @@ impl GildedRose {
                     if item.name != "Backstage passes to a TAFKAL80ETC concert" {
                         if item.quality > 0 {
                             if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality = item.quality - 1;
+                                item.quality -= 1;
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if item.quality < 50 {
-                        item.quality = item.quality + 1;
+                        item.quality += 1;
                     }
                 }
             }
